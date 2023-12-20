122b12686ee7b722fff95886f7420e6827450b4c
Vivek-Anand1999 <vivekanand8867@gmail.com>
2023-12-20 20:50:52 +0530

    origin

--- AlertPage.java
+++ AlertPage.java
@@ -1,8 +1,31 @@
 package com.vivek;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AlertPage {
-    public static void main (String []args){
-        
+    public static void responseAlert(WebDriver driver) {
+        String name = "Rahul";
+        driver.findElement(By.id("name")).sendKeys(name);
+        driver.findElement(By.id("alertbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+
     }
-    
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\vivek\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        // ! Method call strted
+        responseAlert(driver);
+
+    }
+
 }
