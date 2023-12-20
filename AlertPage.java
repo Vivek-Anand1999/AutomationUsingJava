603df2b7d1e8fe45c8a853c02a0030ca54a988bf
Vivek-Anand1999 <vivekanand8867@gmail.com>
2023-12-20 21:16:26 +0530

    cancle Alert

--- AlertPage.java
+++ AlertPage.java
@@ -10,10 +10,12 @@ public class AlertPage {
     public static void responseAlert(WebDriver driver) {
         String name = "Rahul";
         driver.findElement(By.id("name")).sendKeys(name);
-        driver.findElement(By.id("alertbtn")).click();
+        // driver.findElement(By.id("alertbtn")).click();
+        driver.findElement(By.id("confirmbtn")).click();
         System.out.println(driver.switchTo().alert().getText());
-        driver.switchTo().alert().accept();
-
+        // driver.switchTo().alert().accept();//!this for accept /ok/yes or any positive approach
+        driver.switchTo().alert().dismiss();//! this is for cancle no/ or any negative approach
+        
     }
 
     public static void main(String[] args) {
